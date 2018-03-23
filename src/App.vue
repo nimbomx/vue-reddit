f4715f76b3e623349ce5c65a0d79b4ee3b88fa21
Ernesto Hernández <ernesto@nimbo.mx>
2018-03-23 17:50:06 -0600

    Add vue-slideout to add interactivity and responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <div class="menu">
-
-      <div v-for="post in top" :key="post.data.id" @click="selectPost(post)">
+<Slideout  menu="#menu" panel="#panel" padding="300" :toggleSelectors="['.toggle-button']">
+      <nav id="menu">
+        <div v-for="post in top" :key="post.data.id" @click="selectPost(post)">
         Title: {{ post.data.title }} <br>
         Author: {{ post.data.author }} <br>
         Entry Date: {{ post.data.created_utc | date}} <br>
@@ -12,24 +11,32 @@
 
         <hr>
       </div>
-    </div>
-    <div class="detail">
-      Detalle
-      <div v-if="selected">
+      </nav>
+      <main id="panel">
+        <header>
+            <button class="btn btn-secondary toggle-button">☰</button>
+        </header>
+        <div v-if="selected">
         <h4>{{ selected.data.author }}</h4>
         <img :src="selected.data.thumbnail">
         <pre>{{ selected }}</pre>
       </div>
-    </div>
-  </div>
+      </main>
+    </Slideout>
+
 </template>
 
 <script>
 let axios = require('axios')
 
+import Slideout from 'vue-slideout'
 
 export default {
   name: 'app',
+  components:{
+    Slideout,
+
+  },
   data () {
     return {
       top: [],
@@ -58,18 +65,65 @@ export default {
 </script>
 
 <style lang="scss">
-  .menu{
-    float: left;
-    width: 50%;
-    border: 1px solid #eee;
-    padding: 20px;
-    box-sizing: border-box;
+
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
   }
-  .detail{
-    float: right;
-    width: 50%;
-    border: 1px solid #eee;
-    padding: 20px;
-    box-sizing: border-box;
+
+  .slideout-menu {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 300px;
+    height: 100vh;
+    //overflow-y: scroll;
+    overflow: hidden;
+    -webkit-overflow-scrolling: touch;
+    z-index: 0;
+    display: none;
+    background-color: #1D1F20;
+    color: white;
+  }
+
+  .slideout-menu-left {
+    left: 0;
+  }
+
+  .slideout-menu-right {
+    right: 0;
+  }
+
+  .slideout-panel {
+    background-color: white;
+    color: black;
+    position: relative;
+    z-index: 1;
+    will-change: transform;
+    min-height: 100vh;
+    transition: all .5s;
+    overflow: hidden;
+    -webkit-tap-highlight-color: #ccc;
+
+  }
+  .moving{
+    box-shadow: 0px 0px 20px rgba(0,0,0,.5);
+  }
+.toggle-button{
+  margin: 10px;
+}
+  .slideout-open,
+  .slideout-open body,
+  .slideout-open {
+    overflow: hidden;
+  }
+  .slideout-open{
+    .slideout-panel {
+      margin-right: 300px;
+    }
+  }
+  .slideout-open .slideout-menu {
+    display: block;
   }
 </style>
